b512a8225dcce70001a40e1f5bf134e66cdf1d4a
Saksham Goyal <sakshampbt2001@gmail.com>
2023-02-21 11:10:31 +0530

    type changes in reg domain

--- src/main/java/com/hmis/cnt/RegistrationCnt.java
+++ src/main/java/com/hmis/cnt/RegistrationCnt.java
@@ -3,6 +3,7 @@ package com.hmis.cnt;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,6 +14,7 @@ import com.hmis.domain.Registration;
 import com.hmis.service.RegistrationService;
 
 @RestController
+@CrossOrigin("*")
 public class RegistrationCnt {
 	
 	
